a8f17387d9d689b0fcc17fbe5a24f6ceb12e25e5
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-13 14:01:35 +0100

    Improved design

--- components/ProductGroup.jsx
+++ components/ProductGroup.jsx
@@ -8,14 +8,16 @@ const ProductGroup = ({ category, products }) => {
                 <View style={{ height: 30, width: 5, backgroundColor: "#FF5454D9", borderRadius: 4 }} />
                 <Text style={{fontSize: 20, fontWeight: "600", color: "#2A2A2A" }}>{category}</Text>
             </View>
+            <View style={style.productContainer}>
             {products.map((product, index) =>
-            (<View key={index}>
+            (<View key={index} style={style.product}>
                 <Text style={{ color: "#2A2A2A", fontWeight: "600", fontSize: 20 }}>{product.name}</Text>
                 <Text>{product.price}</Text>
                 <Text>{product.rating}</Text>
                 <Text>{product.description}</Text>
             </View>)
             )}
+            </View>
             <View>
             </View>
         </View>
@@ -24,6 +26,19 @@ const ProductGroup = ({ category, products }) => {
 const style = StyleSheet.create({
     container: {
         marginTop: 50
+    },
+    productContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+    },
+    product: {
+        width:"48%",
+        borderWidth: 1,
+        borderColor: "#D9D9D9",
+        borderRadius: 20,
+        padding: 10,
+        marginBottom: 20
     }
 })
 
